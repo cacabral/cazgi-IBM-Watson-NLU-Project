5521a8d316bd9219227d6d60f3d651c0020a65ae
cacabral <ccabral@rocketmail.com>
2021-05-26 13:17:29 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -12,18 +12,25 @@ app.get("/",(req,res)=>{
 
 app.get("/url/emotion", (req,res) => {
 
+    const nlu_I = getNLUInstance();
     return res.send({"happy":"90","sad":"10"});
 });
 
 app.get("/url/sentiment", (req,res) => {
+    
+    const nlu_I = getNLUInstance();
     return res.send("url sentiment for "+req.query.url);
 });
 
 app.get("/text/emotion", (req,res) => {
+    
+    const nlu_I = getNLUInstance();
     return res.send({"happy":"10","sad":"90"});
 });
 
 app.get("/text/sentiment", (req,res) => {
+    
+    const nlu_I = getNLUInstance();
     return res.send("text sentiment for "+req.query.text);
 });
 
@@ -31,3 +38,22 @@ let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+function getNLUInstance(){
+	let api_key = process.env.API_KEY;
+	let api_url = process.env.API_URL;
+	
+	const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+	const { IamAuthenticator } = require('ibm-watson/auth');
+	
+	const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+		version: '2020-08-01',
+		authenticator: new IamAuthenticator({
+			apikey: api_key,
+		}),
+		serviceUrl: api_url,
+	});
+	return naturalLanguageUnderstanding;
+}
